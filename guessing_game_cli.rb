fde0cb6a578fd87307700b40ba5e85ac4dc12c35
Reese M Bowman <reesemb1@gmail.com>
2020-01-27 02:49:29 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,6 @@
 def run_guessing_game
   #puts "Guess a # between 1 and 6: "
-  cpu_guess = rand(1..6)
+  cpu_guess = rand(2..7)
   user_guess = gets.chomp
   user_guess.to_i
   if user_guess == "exit"
