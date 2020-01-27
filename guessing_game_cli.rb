9b50d38c396c86fd8836056848026756c9a2d238
Reese M Bowman <reesemb1@gmail.com>
2020-01-27 03:03:28 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,18 +2,16 @@ def run_guessing_game
 
   cpu_guess = rand(6) + 1
   user_guess = gets.chomp
-  user_guess = user_guess.to_i
-  
   
   if user_guess == "exit"
     puts "Goodbye!"
   end
   
-  if user_guess == cpu_guess
+  if user_guess.to_i == cpu_guess
     puts "You guessed the correct number!"
   end
   
-  if user_guess != cpu_guess
+  if user_guess.to_i != cpu_guess
     puts "Sorry! The computer guessed #{cpu_guess}."
   end
   
